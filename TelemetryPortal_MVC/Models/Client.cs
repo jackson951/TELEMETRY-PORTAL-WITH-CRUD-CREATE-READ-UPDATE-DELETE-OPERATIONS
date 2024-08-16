3d88eb70809463c30ffbd3c90030e427bf96ad66
Mj Khuto <39990966@mynwu.ac.za>
2024-08-16 23:02:06 +0200

    Add project files.

---
+++ TelemetryPortal_MVC/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TelemetryPortal_MVC.Models;
+
+public partial class Client
+{
+    [DisplayName("Client ID")]
+    public Guid ClientId { get; set; }
+
+    [DisplayName("Client Name")] 
+    public string? ClientName { get; set; }
+
+    [DisplayName("Primary Contact Email")]
+    [EmailAddress]
+    public string? PrimaryContactEmail { get; set; }
+
+    [DisplayName("Date Onboarded")]
+    public DateTime? DateOnboarded { get; set; }
+}
---
+++ TelemetryPortal_MVC/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace TelemetryPortal_MVC.Models;
+
+public partial class Project
+{
+    [DisplayName("Project ID")]
+    public Guid ProjectId { get; set; }
+
+    [DisplayName("Project Name")]
+    public string? ProjectName { get; set; }
+
+    [DisplayName("Project Description")]
+    public string? ProjectDescription { get; set; }
+
+    [DisplayName("Project Creation Date")]
+    public DateTime? ProjectCreationDate { get; set; }
+
+    [DisplayName("Project Status")]
+    public string? ProjectStatus { get; set; }
+
+    [DisplayName("Client ID")]
+    public Guid? ClientId { get; set; }
+}
